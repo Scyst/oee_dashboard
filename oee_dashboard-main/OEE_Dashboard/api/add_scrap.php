6647ff4cdef4caf76267047365cd07a8939d182c
Scyst <91644065+Scyst@users.noreply.github.com>
2025-05-26 10:41:16 +0700

    Update add_scrap.php

--- oee_dashboard-main/OEE_Dashboard/api/add_scrap.php
+++ oee_dashboard-main/OEE_Dashboard/api/add_scrap.php
@@ -1,32 +1,26 @@
 <?php
 require_once("../db.php");
 
-$log_date = $_POST['log_date'];
-$shift = $_POST['shift'];
-$log_time = $_POST['log_time'];
-$part_no = $_POST['part_no'];
-$scrap_count = $_POST['scrap_count'];
+$data = json_decode(file_get_contents("php://input"), true);
 
-$params = [$log_date, $shift, $log_time, $part_no];
-$checkSql = "SELECT * FROM scrap_logs WHERE log_date = ? AND shift = ? AND log_time = ? AND part_no = ?";
-$stmt = sqlsrv_query($conn, $checkSql, $params);
+$log_date = $data['log_date'];
+$log_time = $data['log_time'];
+$line = $data['line'];
+$model = $data['model'];
+$part_no = $data['part_no'];
+$scrap_type = $data['scrap_type'];
+$scrap_count = intval($data['scrap_count']);
 
-if ($stmt === false) {
-    die(print_r(sqlsrv_errors(), true));
-}
+$sql = "INSERT INTO scrap (log_date, log_time, line, model, part_no, scrap_type, scrap_count)
+        VALUES (?, ?, ?, ?, ?, ?, ?)";
+$params = [$log_date, $log_time, $line, $model, $part_no, $scrap_type, $scrap_count];
 
-if (sqlsrv_has_rows($stmt)) {
-    $updateSql = "UPDATE scrap_logs SET scrap_count = scrap_count + ?
-                  WHERE log_date = ? AND shift = ? AND log_time = ? AND part_no = ?";
-    $updateParams = [$scrap_count, $log_date, $shift, $log_time, $part_no];
-    sqlsrv_query($conn, $updateSql, $updateParams);
-    echo json_encode(["status" => "success", "message" => "Scrap data updated"]);
+$stmt = sqlsrv_query($conn, $sql, $params);
+if ($stmt) {
+    echo json_encode(["status" => "success"]);
 } else {
-    $insertSql = "INSERT INTO scrap_logs (log_date, shift, log_time, part_no, scrap_count)
-                  VALUES (?, ?, ?, ?, ?)";
-    $insertParams = [$log_date, $shift, $log_time, $part_no, $scrap_count];
-    sqlsrv_query($conn, $insertSql, $insertParams);
-    echo json_encode(["status" => "success", "message" => "Scrap data inserted"]);
+    echo json_encode(["status" => "error", "message" => sqlsrv_errors()]);
 }
 ?>
+
 
