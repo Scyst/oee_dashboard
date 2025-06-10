e68aca7da5f4c25bb50c32d93e28f3a835234b44
Scyst <napat_33025@hotmail.com>
2025-06-10 14:08:41 +0700

    Final (Maybe) V0.2 14:08 10/06/25

--- oee_dashboard-main/OEE_Dashboard/api/db.php
+++ oee_dashboard-main/OEE_Dashboard/api/db.php
@@ -2,8 +2,8 @@
 $serverName = "LAPTOP-E0M0G0I9";  // or use "localhost\SQLEXPRESS" if using SQLEXPRESS
 $connectionOptions = array(
     "Database" => "oee_db",   // your database name
-    "Uid" => "verymaron01",                    // your SQL Server username
-    "PWD" => "numthong01",         // your SQL Server password
+    "Uid" => "verymaron01",   // your SQL Server username
+    "PWD" => "numthong01",    // your SQL Server password
     "CharacterSet" => "UTF-8"
 );
 
