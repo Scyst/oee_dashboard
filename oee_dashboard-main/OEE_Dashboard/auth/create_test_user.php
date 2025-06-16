462e4efc8996e765d8c04a26c3514b6c780d5833
Scyst <napat_33025@hotmail.com>
2025-06-16 13:48:45 +0700

    update user 13:48 16/06/25

--- oee_dashboard-main/OEE_Dashboard/auth/create_test_user.php
+++ oee_dashboard-main/OEE_Dashboard/auth/create_test_user.php
@@ -1,10 +1,26 @@
 <?php
 require_once("../api/db.php");
 
-$username = 'admin';
-$password = password_hash('admin123', PASSWORD_DEFAULT);
-$sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-$stmt = sqlsrv_query($conn, $sql, [$username, $password]);
+$users = [
+    ['verymaron01', 'numthong01'],
+    ['SncToolbox', 'SncToolbox@2025'],
+    ['Assembly', 'Assembly@2025'],
+    ['Spot', 'Spot@2025'],
+    ['Bend', 'Bend@2025'],
+    ['Laser', 'Laser@2025'],
+    ['Paint', 'Paint@2025'],
+    ['Press', 'Press@2025'],
+];
 
-echo $stmt ? "User created." : "Error: " . print_r(sqlsrv_errors(), true);
+foreach ($users as $user) {
+    $username = $user[0];
+    $password = password_hash($user[1], PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $stmt = sqlsrv_query($conn, $sql, [$username, $password]);
+    if ($stmt) {
+        echo "User {$username} created.<br>";
+    } else {
+        echo "Error creating {$username}: " . print_r(sqlsrv_errors(), true) . "<br>";
+    }
+}
 ?>
